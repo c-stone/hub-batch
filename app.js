bc87611e6fe47599f313827f6112fec0698448f6
Christopher Stone <cstone@bosmmms1ljxh3qf.hubspot.local>
2016-08-26 16:17:46 -0400

    successfully exporting CSV files. Planing to refactor next

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-var request = require("request"),
-    fs = require("fs");
+var fs = require("fs"),
+    request = require("request"),
+    json2csv = require('json2csv');
 
 // Access token for the portal you would like to get page/posts from
-var accessToken = '2b7836c2-2862-4ac8-b3e8-bf0d1eea8ba5',
-    cosContentType = 'pages', // OR 'blog-posts'
+var accessToken = '20897922-d738-41fc-9108-213a02f152da',
+    cosContentType = 'blog-posts', // OR 'blog-posts'
     exportType = 'JSON', // OR 'CSV'
     queryString = {
       access_token: accessToken,
       // Optional Parameters
-      // limit: 20,
+      limit: 3000,
       // offset: 0,
       // archived: false,
       // blog_author_id: 34623,
       // campaign: 3549384759,
-      // content_group_id: 8743928374, // A specfic blog's *blog only*
+      content_group_id: 3345520891, // A specfic blog's *blog only*
       // created__gt: 4329847200000, // Supports exact, range, gt, gte, lt, lte
       // deleted_at__lt: 34572630000,
       // publish_date: 542376570000,
       // updated: 793847290000,
-      // name__contains: 'Great Page Name', //Supports contains, icontains, ne
+      name__icontains: 'prospect', //Supports contains, icontains, ne
       // slug: 'sales/article/great-page-name', // Supports exact, in
       // subcategory: 'site_page', // OR landing_page
       // state: 'DRAFT'; // OR PUBLISHED, SCHEDULED *blog only*
@@ -32,6 +33,7 @@ function getContentIds(exportType) {
     headers: {'cache-control': 'no-cache'}
   };
 
+//Turn this into a function with arguments
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
     if (response.statusCode !== 200) {
@@ -39,7 +41,7 @@ function getContentIds(exportType) {
     }
 
     var parsedBody = JSON.parse(body);
-    if (exportType === 'JSON' || 'json') {
+    if (exportType === 'JSON') {
       var cosContentJson = parsedBody.objects.map(function(object) {
         return  {
           name: object.name,
@@ -47,22 +49,32 @@ function getContentIds(exportType) {
           id: object.analytics_page_id
         };
       });//Optional .filter();
-      fs.appendFile('message6.json', JSON.stringify(cosContentJson), function (err) {
-        if(err) {
-          return console.log(err);
-        }
-        console.log("The file was saved!");
+
+      var fields = ['name', 'slug', 'id'];
+      var csv = json2csv({ data: cosContentJson, fields: fields });
+
+      fs.writeFile('test-file.csv', csv, function(err) {
+        if (err) throw err;
+        console.log('file saved');
       });
-      console.log(cosContentJson);
-    } else if (exportType === 'CSV' || 'csv') {
+
+      fs.writeFile('message6.json', JSON.stringify(cosContentJson),
+        function (err) {
+          if(err) {
+            return console.log(err);
+          }
+          console.log("The JSON file was saved!");
+        });
+    } else if (exportType === 'CSV') {
       var cosContentCsv = parsedBody.objects.map(function(object) {
-        return  object.name, object.slug, object.analytics_page_id;
+        return  object.name + ',' + object.slug + ',' + object.analytics_page_id + '\n';
       });//Optional .filter();
-      fs.appendFile('message6.csv', JSON.stringify(cosContentCsv), function (err) {
+
+      fs.writeFile('message9.csv', JSON.stringify(cosContentCsv), function (err) {
         if(err) {
           return console.log(err);
         }
-        console.log("The file was saved!");
+        console.log("The CSV file was saved!");
       });
       console.log(cosContentCsv);
     }
