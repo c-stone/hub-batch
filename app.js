7d462f02e28ba270cd85b2d0ab62e69bdb629567
Chris Stone <cstone@hubspot.com>
2016-08-29 09:37:54 -0400

    added a gitignore file

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ var accessToken = '20897922-d738-41fc-9108-213a02f152da',
     queryString = {
       access_token: accessToken,
       // Optional Parameters
-      limit: 3000,
+      // limit: 3000,
       // offset: 0,
       // archived: false,
       // blog_author_id: 34623,
       // campaign: 3549384759,
-      content_group_id: 3345520891, // A specfic blog's *blog only*
+      // content_group_id: 3345520891, // A specfic blog's *blog only*
       // created__gt: 4329847200000, // Supports exact, range, gt, gte, lt, lte
       // deleted_at__lt: 34572630000,
       // publish_date: 542376570000,
       // updated: 793847290000,
-      name__icontains: 'prospect', //Supports contains, icontains, ne
+      // name__icontains: 'prospect', //Supports contains, icontains, ne
       // slug: 'sales/article/great-page-name', // Supports exact, in
       // subcategory: 'site_page', // OR landing_page
       // state: 'DRAFT'; // OR PUBLISHED, SCHEDULED *blog only*
